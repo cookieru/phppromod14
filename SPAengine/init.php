70d5e07c09d375ce74c3cd1bbeb23c40484047bf
cookieru <ilyasuk00@mail.ru>
2024-03-04 17:25:02 +0500

    add authentication methods

---
+++ SPAengine/init.php
@@ -0,0 +1,12 @@
+<?php
+$enableInit = false;
+if ($enableInit) {
+    $userlist = array();
+    $user = ["password" => sha1("123456"), "birthday" => strtotime("2000-03-20")];
+    $userlist["cookieru"] = $user;
+    $user = ["password" => sha1("234567"), "birthday" => strtotime("1990-06-12")];
+    $userlist["nastya"] = $user;
+
+    $json_out = json_encode($userlist);
+    file_put_contents("data/secrets.json", $json_out);
+}
---
+++ SPAengine/methods.php
@@ -0,0 +1,54 @@
+<?php
+//функция getUsersList() возвращает массив всех пользователей и хэшей их паролей;
+function getUserList()
+{
+    $json_userlist = file_get_contents("data/secrets.json");
+    if ($json_userlist) {
+        return json_decode($json_userlist, true);
+    } else {
+        //require_once "init.php";
+        //return getUserList();
+
+        throw new Exception("Не удалось загрузить данные пользователей");
+    }
+}
+//var_dump(getUserList());
+
+//функция existsUser($login) проверяет, существует ли пользователь с указанным логином;
+function existsUser($login)
+{
+    $userlist = getUserList();
+
+    if (isset($userlist[$login]))
+        return true;
+
+    return false;
+}
+//var_dump(existsUser("cookieru"));
+//var_dump(existsUser("cookie"));
+
+//функция checkPassword($login, $password) пусть возвращает true тогда, когда существует 
+//пользователь с указанным логином и введенный им пароль прошел проверку, иначе — false;
+function checkPassword($login, $password)
+{
+    $userlist = getUserList();
+
+    if (isset($userlist[$login])) {
+        $pass_hash = sha1($password);
+        return($pass_hash == $userlist[$login]["password"]);
+    }
+
+    return false;
+}
+//var_dump(checkPassword("cookieru", "123456"));
+//var_dump(checkPassword("cookieru", "654321"));
+
+//функция getCurrentUser() которая возвращает либо имя вошедшего на сайт пользователя, либо null.
+function getCurrentUser()
+{
+    if (isset($_SESSION["current_user"]))
+        return $_SESSION["current_user"];
+
+    return null;
+}
+//var_dump(getCurrentUser());
